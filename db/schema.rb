6ff0136999c24e5072d26cb54ddc27546f3fbc63
MaximuSSmile <MaximusGrandDeveloper@gmail.com>
2016-08-04 06:34:12 +0300

    heroku cry 2

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160803090351) do
+ActiveRecord::Schema.define(version: 20160803090358) do
 
   create_table "additional_educations", force: :cascade do |t|
     t.text     "academy"
